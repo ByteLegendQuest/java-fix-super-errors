a079149066d26de7b900f7e7fde607be56904a4c
greek-zzf <49605925+greek-zzf@users.noreply.github.com>
2022-08-05 08:59:16 +0000

    Update at 2022-08-05T08:59:16.337824Z

--- src/main/java/com/bytelegend/CleanCat.java
+++ src/main/java/com/bytelegend/CleanCat.java
@@ -1,13 +1,27 @@
 package com.bytelegend;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 /** `CleanCat` inherits from `Cat` class, but it only eats clean food (`food.isClean()`). */
 public class CleanCat extends Cat {
-    public CleanCat(String name) {}
+    public CleanCat(String name) {
+        super(name);
+    }
 
     @Override
     public void eat(Food food) {
-        if (!food.isClean()) {
-            eat(food);
+        if (food.isClean()) {
+            super.eat(food);
         }
+    }
+
+    @Override
+    public void eat(List<Food> foods) {
+        List<Food> cleanFoods = foods.stream()
+                .filter(Food::isClean)
+                .collect(Collectors.toList());
+
+        super.eat(cleanFoods);
     }
 }
