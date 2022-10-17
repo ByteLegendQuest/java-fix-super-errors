5f543dffac072216b946a9979de4a6761132480d
pipiloveslife <Pain_Zzz@163.com>
2022-10-17 08:53:41 +0000

    Update at 2022-10-17T08:53:40.998797Z

--- src/main/java/com/bytelegend/CleanCat.java
+++ src/main/java/com/bytelegend/CleanCat.java
@@ -1,20 +1,23 @@
 package com.bytelegend;
 
 import java.util.stream.Collectors;
+import java.util.List;
 
 /** `CleanCat` inherits from `Cat` class, but it only eats clean food (`food.isClean()`). */
 public class CleanCat extends Cat {
-    public CleanCat(String name) {}
+    public CleanCat(String name) {
+        super(name);
+    }
 
     @Override
     public void eat(Food food) {
         if (food.isClean()) {
-            eat(food);
+            super.eat(food);
         }
     }
 
     @Override
     public void eat(List<Food> foods) {
-        eatenFoods.addAll(foods.stream().filter(f -> f.isClean()).collect(Collectors.toList()));
+        super.eat(foods.stream().filter(Food::isClean).collect(Collectors.toList()));
     }
 }
