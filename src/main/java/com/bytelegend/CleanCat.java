67d9f7fc25d9320e7536024862b99bdc711d8a07
jenny-2233 <822204051@qq.com>
2022-12-01 13:45:05 +0000

    Update at 2022-12-01T13:45:05.887038Z

--- src/main/java/com/bytelegend/CleanCat.java
+++ src/main/java/com/bytelegend/CleanCat.java
@@ -3,13 +3,13 @@ package com.bytelegend;
 /** `CleanCat` inherits from `Cat` class, but it only eats clean food (`food.isClean()`). */
 public class CleanCat extends Cat {
     public CleanCat(String name) {
-         super(name);
+        super(name);
     }
 
     @Override
     public void eat(Food food) {
-        if (!food.isClean()) {
-            eat(food);
+        if (food.isClean()) {
+            super.eat(food);
         }
     }
 }
