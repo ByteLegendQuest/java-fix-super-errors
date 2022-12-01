8752660ce62417182a93fe5192851d08484e2bab
pamaomaoa <544060246@qq.com>
2022-12-01 13:36:40 +0000

    Update at 2022-12-01T13:36:40.091807Z

--- src/main/java/com/bytelegend/CleanCat.java
+++ src/main/java/com/bytelegend/CleanCat.java
@@ -1,4 +1,6 @@
 package com.bytelegend;
+import java.util.ArrayList;
+import java.util.List;
 
 /** `CleanCat` inherits from `Cat` class, but it only eats clean food (`food.isClean()`). */
 public class CleanCat extends Cat {
@@ -11,5 +13,16 @@ public class CleanCat extends Cat {
         if (food.isClean()) {
             super.eat(food);
         }
+    }
+
+     @Override
+    public void eat(List<Food> foods) {
+        List<Food> list = new ArrayList<>();
+        for(Food f : foods)
+        {
+            if(f.isClean())
+                list.add(f);
+        }
+        super.eat(list);
     }
 }
