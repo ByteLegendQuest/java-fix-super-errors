5966d8c25e84471d0cdf3ed8095a431c503c3743
williamgl <ligan@oregonstate.edu>
2023-07-09 21:51:20 -0700

    Update

--- src/main/java/com/bytelegend/CleanCat.java
+++ src/main/java/com/bytelegend/CleanCat.java
@@ -1,13 +1,27 @@
 package com.bytelegend;
 
+
+import java.util.List;
+
 /** `CleanCat` inherits from `Cat` class, but it only eats clean food (`food.isClean()`). */
 public class CleanCat extends Cat {
-    public CleanCat(String name) {}
+    public CleanCat(String name) {
+        super(name);
+    }
 
     @Override
     public void eat(Food food) {
         if (!food.isClean()) {
-            eat(food);
+            super.eat(food);
+        }
+    }
+
+    @Override
+    public void eat(List<Food> foods) {
+        for (Food food: foods) {
+            if (food.isClean()) {
+                eat(food);
+            }
         }
     }
 }
